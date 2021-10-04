dbf4b8f612e6369181aa49110000b8e595870e8a
Federico <federicodebona@hotmail.it>
2021-10-04 22:21:06 +0200

    .

--- config.lua
+++ config.lua
@@ -2,18 +2,18 @@ Config = {}
 Config.WaitTimeAtBusStop = 10 -- In seconds
 
 Config.Routes = {
-	{ -- First spawn point - Vechileshop - Central Garage
+	{ 	-- First spawn point - Vechileshop - Central Garage
 		info = { 
 			color = 84, 
 			hash = "bus",
-			busNum = 4, -- >= 1
+			busNum = 4, -- busNum >= 1
 			timeBetweenBus = 40, -- In second 
 		},
 		{ pos = vector3(234.9626, -829.2527, 29.98755), heading = 68.031, stop = true },
 		--{ pos = vector3(-239.7231, -1146.936, 22.62415), heading = 272.125, stop = false }, -- IF UNCOMMENTED THE GAME WILL CRASH (not sure anymore)
 		{ pos = vector3(-232.1934, -983.7758, 28.60583), heading = 158.740, stop = true },
 		{ pos = vector3(-68.75604, -1078.668, 26.97144), heading = 340.15, stop = true },
-		{ pos = vector3(176.8747, -1030.365, 29.3136), heading = 0.0, stop = false }, -- stop = false, bus won't stop but will force it to pass this point
+		{ pos = vector3(176.8747, -1030.365, 29.3136), heading = 0.0, stop = false },
 		{ pos = vector3(270.3956, -848.2022, 29.33044), heading = 70.866142272949, stop = false },
 	},
 	--Example of new route use /busstop to get a bus stop ready to be pasted here 
